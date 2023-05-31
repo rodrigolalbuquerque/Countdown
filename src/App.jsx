699f2de2118217d2b8931037a0ddfbc6a6b8b544
Rodrigo Lomar de Albuquerque <rodrigolomardev@gmail.com>
2023-05-31 19:50:55 -0300

    sets bg, gif img, and countdown position

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import './app.css'
+import s2 from './assets/s2.gif'
 
 function App() {
   return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center bg-gradient-radial from-orange-200 to-red-300'>
+      <img src={s2} className='relative bottom-[15%]' />
+      <div className='relative bottom-[20%] text-6xl'>Countdown time</div>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
+
